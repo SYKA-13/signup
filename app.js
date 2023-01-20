eb956fb38c8fe7ab9094bbe180691d7b401ddcd0
kalam <syedkalam13@gmail.com>
2023-01-20 21:51:04 +0530

    commit

--- app.js
+++ app.js
@@ -27,8 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-mongoose.set("useCreateIndex", true);
+
 
 const userSchema = new mongoose.Schema ({
   email: String,
